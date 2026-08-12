e8ca57bf21d01030477dbed1a3a37e4f1a4bfc28
douglas86 <douglasmaxton@gmail.com>
2026-08-12 19:12:56 +0100

    created a dark green button in top right hand corner

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,16 +1,47 @@
 use iced::Element;
-use iced::widget::{container, text};
+use iced::widget::{Column, button, container};
+use iced::{Alignment, Color, Length};
 
 #[derive(Default)]
 struct State;
 
 #[derive(Debug, Clone)]
-enum Message {}
+enum Message {
+    ButtonClicked,
+}
 
-fn update(_state: &mut State, _message: Message) {}
+fn update(_state: &mut State, message: Message) {
+    match message {
+        Message::ButtonClicked => {
+            println!("button clicked!");
+        }
+    }
+}
 
 fn view(_state: &State) -> Element<'_, Message> {
-    container(text("Hello World!")).into()
+    let button = button("Click Me!")
+        .on_press(Message::ButtonClicked)
+        .style(|_theme, _status| button::Style {
+            background: Some(iced::Background::Color(Color::from_rgb(
+                19.0 / 255.0,
+                82.0 / 255.0,
+                36.0 / 255.0,
+            ))),
+            text_color: Color::WHITE,
+            border: iced::Border {
+                radius: 6.0.into(),
+                ..iced::Border::default()
+            },
+            ..button::Style::default()
+        });
+
+    container(Column::new().push(button))
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .align_x(Alignment::End)
+        .align_y(Alignment::Start)
+        .padding(20)
+        .into()
 }
 
 fn main() -> iced::Result {
