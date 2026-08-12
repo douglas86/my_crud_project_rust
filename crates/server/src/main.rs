490d038d57a0ddf139b7cc74fa2d098698b090be
douglas86 <douglasmaxton@gmail.com>
2026-08-12 18:39:27 +0100

    added axum and tokio to server and printed hello world to localhost

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+
+async fn get_handler() -> &'static str {
+    "hello world"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(get_handler));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Listening on http://localhost:3000");
+
+    axum::serve(listener, app).await.unwrap();
 }
--- crates/shared/src/lib.rs
+++ crates/shared/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use serde::{Deserialize, Serialize};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Task {
+    pub id: Option<String>,
+    pub title: String,
+    pub completed: bool,
 }
