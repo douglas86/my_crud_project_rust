abcc23a6cd62c59897e829a0a397b0d63a5bb7ca
douglas86 <douglasmaxton@gmail.com>
2026-08-12 18:20:34 +0100

    added iced and shows hello world text on load

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use iced::Element;
+use iced::widget::{container, text};
+
+#[derive(Default)]
+struct State;
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+fn update(_state: &mut State, _message: Message) {}
+
+fn view(_state: &State) -> Element<'_, Message> {
+    container(text("Hello World!")).into()
+}
+
+fn main() -> iced::Result {
+    iced::run(update, view)
 }
